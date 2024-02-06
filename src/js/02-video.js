eb2ba65aecb4c877d14a989910ebb806e228ddc9
Monika-Lis <lis.monikak@gmail.com>
2024-02-06 13:20:42 +0000

    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -11,7 +12,7 @@ const saveTime = data => {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', saveTime);
+player.on('timeupdate', throttle(saveTime, 1000));
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
 
