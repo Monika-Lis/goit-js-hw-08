367b46e89ae910901ab5a29497dd4216b42fb002
Monika-Lis <lis.monikak@gmail.com>
2024-02-06 13:14:23 +0000

    task2 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,23 @@
+import Player from '@vimeo/player';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
 });
+
+const saveTime = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', saveTime);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
