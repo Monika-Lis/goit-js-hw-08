cc8e28f79a63e45d2327f7fd86e94bc54cdaa4ef
Monika-Lis <lis.monikak@gmail.com>
2024-02-06 14:05:15 +0000

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+const saveFormData = () => {
+  formData.email = feedbackForm.email.value;
+  formData.message = feedbackForm.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (formData) {
+    feedbackForm.email.value = formData.email || '';
+    feedbackForm.message.value = formData.message || '';
+  }
+});
+
+feedbackForm.addEventListener('input', throttle(saveFormData, 500));
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  feedbackForm.reset();
+});
